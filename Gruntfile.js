3e96f9badb4739251d1c32686bb4c9fcf81cba2c
tommykahlow <tommy.kahlow@truematter.com>
2014-07-25 17:20:42 -0400

    now we're cooking

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,31 @@ module.exports = function(grunt) {
     grunt.initConfig({
         jshint: {
             all: ['Gruntfile.js', 'js/*.js']
-        }
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    'css/app.css': 'sass/app.sass',
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['*.js', 'js/*.js', 'sass/*.sass'],
+                tasks: ['jshint', 'sass'],
+                options: {
+                    spawn: false,
+                },
+            },
+        },
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['sass']);
 };
