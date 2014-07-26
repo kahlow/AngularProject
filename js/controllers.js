994d7d2f521ded78977bb9430ed7d25a6c86b7f3
tommykahlow <tommy.kahlow@truematter.com>
2014-07-26 10:33:30 -0400

    edit button added with ability to select row

--- js/controllers.js
+++ js/controllers.js
@@ -2,14 +2,28 @@ var movieApp = angular.module('movieApp', ['ngRoute', 'ngGrid', 'restangular']);
 
 movieApp.controller('MovieListCtrl', function ($scope, Restangular) {
 
+    // GET request for movie list
     $scope.movieList = Restangular.all('movies').getList().$object;
 
+    // setting up the ability to get selected items from grid
+    $scope.selectedMovie = [];
+
+    // initialize the grid
     $scope.movies = { 
         data: 'movieList', 
         columnDefs: [{field:'name', displayName:'Name'}, 
                     {field:'description', displayName:'Description'},
                     {field:'director', displayName:'Director'},
                     {field:'releaseYear', displayName:'Year Released'}],
-        multiSelect: false
+        multiSelect: false,
+        selectedItems: $scope.selectedMovie,
+        afterSelectionChange: function() {
+            $('.btn').prop('disabled', false);
+        }
+    };
+
+    // edit button click event
+    $scope.edit = function(){
+        console.log($scope.selectedMovie[0]);    
     };
 });
