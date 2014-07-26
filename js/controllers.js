5f8319af3bcbcc5a2ad0ec05553d518125b6521d
tommykahlow <tommy.kahlow@truematter.com>
2014-07-25 20:15:06 -0400

    adding node

--- js/controllers.js
+++ js/controllers.js
@@ -1,14 +1,32 @@
-var movieApp = angular.module('movieApp', ['ngGrid']);
+var movieApp = angular.module('movieApp', ['ngGrid', 'restangular']);
 
-movieApp.controller('MovieListCtrl', function ($scope) {
-  $scope.myData = [{
-        name: 'Terminator 2',
-        description: 'He\'s back.'
-    },
-    {
-        name: 'Big Trouble in Little China',
-        description: 'Don\'t mess with Jack Burton'
-    }];
+movieApp.controller('MovieListCtrl', function ($scope, Restangular) {
 
-    $scope.movies = { data : 'myData' };
+    // Does a GET to /accounts
+    // Returns an empty array by default. Once a value is returned from the server
+    // that array is filled with those values. So you can use this in your template
+    $scope.movies = Restangular.all('127.0.0.1:1337').getList().$object;
+
+
+
+
+
+
+
+
+
+  // $scope.myData = [{
+  //       name: 'Terminator 2',
+  //       description: 'He\'s back.'
+  //   },
+  //   {
+  //       name: 'Big Trouble in Little China',
+  //       description: 'Don\'t mess with Jack Burton'
+  //   },
+  //   {
+  //       name: 'Evil Dead',
+  //       description: 'Boomstick'
+  //   }];
+
+  //   $scope.movies = { data : 'myData' };
 });
