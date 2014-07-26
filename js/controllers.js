90b188eb3021ad437a34836e072762eaca012937
tommykahlow <tommy.kahlow@truematter.com>
2014-07-26 08:14:05 -0400

    I think this is going to work

--- js/controllers.js
+++ js/controllers.js
@@ -2,31 +2,28 @@ var movieApp = angular.module('movieApp', ['ngRoute', 'ngGrid', 'restangular']);
 
 movieApp.controller('MovieListCtrl', function ($scope, Restangular) {
 
-  
+    $scope.movies = Restangular.all('movies').getList().$object;
 
-    Restangular.all('movies').getList().then(function(movieList) {
-        //$scope.myData = movieList;
-        console.log(movieList[0]);
-        
-    });
+    console.log($scope.movies);
 
+   
+   // $scope.myData = [{
+   //          name: 'Terminator 2',
+   //          description: 'He\'s back.'
+   //      },
+   //      {
+   //          name: 'Big Trouble in Little China',
+   //          description: 'Don\'t mess with Jack Burton'
+   //      },
+   //      {
+   //          name: 'Evil Dead',
+   //          description: 'Boomstick'
+   //      }];
 
+   //      $scope.movies = { data : 'myData' };
+    
 
    
 
-  $scope.myData = [{
-        name: 'Terminator 2',
-        description: 'He\'s back.'
-    },
-    {
-        name: 'Big Trouble in Little China',
-        description: 'Don\'t mess with Jack Burton'
-    },
-    {
-        name: 'Evil Dead',
-        description: 'Boomstick'
-    }];
-
-    $scope.movies = { data : 'myData' };
     
 });
