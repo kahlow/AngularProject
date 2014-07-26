8fe01acd9124b3162164122018c15d79dc5c3443
tommykahlow <tommy.kahlow@truematter.com>
2014-07-26 08:17:43 -0400

    Displaying json list to ngGrid!

--- js/controllers.js
+++ js/controllers.js
@@ -2,28 +2,10 @@ var movieApp = angular.module('movieApp', ['ngRoute', 'ngGrid', 'restangular']);
 
 movieApp.controller('MovieListCtrl', function ($scope, Restangular) {
 
-    $scope.movies = Restangular.all('movies').getList().$object;
+    $scope.movieList = Restangular.all('movies').getList().$object;
 
-    console.log($scope.movies);
-
-   
-   // $scope.myData = [{
-   //          name: 'Terminator 2',
-   //          description: 'He\'s back.'
-   //      },
-   //      {
-   //          name: 'Big Trouble in Little China',
-   //          description: 'Don\'t mess with Jack Burton'
-   //      },
-   //      {
-   //          name: 'Evil Dead',
-   //          description: 'Boomstick'
-   //      }];
-
-   //      $scope.movies = { data : 'myData' };
-    
-
-   
-
-    
+    $scope.movies = { 
+        data: 'movieList', 
+        columnDefs: [{field:'name', displayName:'Name'}, {field:'description', displayName:'Description'}]
+    };
 });
