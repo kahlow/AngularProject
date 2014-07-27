592c2199c18e1f59d785e6c06f1336b0c8ea8fd1
tommykahlow <tommy.kahlow@truematter.com>
2014-07-27 11:26:41 -0400

    resolving a promise

--- js/controllers.js
+++ js/controllers.js
@@ -18,15 +18,21 @@ movieApp.config(['$routeProvider',
 
 movieApp.controller('MovieListCtrl', function ($scope, $location, movieListService) {
 
-    // GET request for movie list
-    $scope.movieList = movieListService.getMovieList.$object;
-
     // setting up the ability to get selected items from grid
     $scope.selectedMovie = [];
+    
+    $scope.myData = [];
 
+    // GET request for movie list
+    movieListService.getMovieList.then(function (movieList) {
+        _.each(movieList, function (elem){
+            $scope.myData.push(elem);
+        });
+    });
+     
     // initialize the grid
     $scope.movies = { 
-        data: 'movieList', 
+        data: 'myData', 
         columnDefs: [{field:'title', displayName:'Title'}, 
                     {field:'description', displayName:'Description'},
                     {field:'director', displayName:'Director'},
@@ -37,7 +43,7 @@ movieApp.controller('MovieListCtrl', function ($scope, $location, movieListServi
             $('.btn').prop('disabled', false);
         }
     };
-
+   
     // I have a feeling there's a better way to handle this
     $scope.edit = function() {
         $location.url('/edit/' + $scope.selectedMovie[0].id);
@@ -45,12 +51,10 @@ movieApp.controller('MovieListCtrl', function ($scope, $location, movieListServi
 });
 
 movieApp.controller('MovieDetailCtrl', function ($scope, $routeParams, $location, movieListService) {
-    // I have a feeling there's a better way to handle this too
+
     movieListService.getMovieList.then(function (movieList) {
-        movieList.forEach(function (elem){
-            if($routeParams.movieId == elem.id){
-                $scope.movie = elem;
-            }
+        $scope.movie = _.find(movieList, function (m) { 
+            return m.id == $routeParams.movieId;
         });
     });
 
--- js/services.js
+++ js/services.js
@@ -3,5 +3,5 @@ var movieServices = angular.module('movieServices', ['restangular']);
 movieServices.factory('movieListService', ['Restangular', function(Restangular) {
     return {
         getMovieList:  Restangular.all('movies').getList()
-    }
+    };
 }]);
